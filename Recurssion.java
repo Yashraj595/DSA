c26d9b904b3b6fd35935edf783957331bc5474f5
Yashraj595 <ys2391356@gmail.com>
2025-12-14 21:41:00 +0530

    updating new Question

---
+++ Recurssion.java
@@ -0,0 +1,29 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class Recurssion {
+
+  public static int factorial(int n) {
+
+    if (n == 0) {
+      return 1;
+    }
+
+    int factNminus1 = factorial(n - 1);
+    int fact = n * factNminus1;
+
+    return fact;
+  }
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+
+    int n = sc.nextInt();
+    int ans = factorial(n);
+
+    System.out.println("Factorial is: " + ans);
+  }
+}
